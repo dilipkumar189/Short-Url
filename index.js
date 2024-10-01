c36fc058b22d49456566ceff90f6c05b76b3bff0
Dilip Kumar <127296438+dilipkumar189@users.noreply.github.com>
2024-10-01 18:59:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const URL = require("./models/url");
 const urlRoute = require("./routes/url");
 const app = express();
 
-const PORT = 5000;
+const PORT = 8000;
 
 connectDb("mongodb://127.0.0.1:27017/short-url");
 app.use(express.json());
